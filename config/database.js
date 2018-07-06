97f9e4813c1d6e2e0e76df39599cdf913c35507b
dhofa <ahmadmundhofa@gmail.com>
2018-07-06 21:08:47 +0700

    tes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 //  url: 'mongodb://admin:admin123@rmvts-shard-00-00-kqh55.mongodb.net:27017,rmvts-shard-00-01-kqh55.mongodb.net:27017,rmvts-shard-00-02-kqh55.mongodb.net:27017/test?ssl=true&replicaSet=RMVTS-shard-0&authSource=admin'
- url: 'mongodb://dhofa:bismillahrmvts@jagopesan.com:64526/test'
-//  url: 'mongodb://dhofa:rmvts.localhost:64526/rmvts'
+ url: 'mongodb://dhofa:bismillahrmvts@jagopesan.com:64526/rmvts'
+//  url: 'mongodb://dhofa:bismillahrmvts@jagopesan.com:64526/rmvts'
 }
