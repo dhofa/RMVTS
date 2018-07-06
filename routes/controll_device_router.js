57901c4ec70188e7d429c8f3479c5fcf94f13754
dhofa <ahmadmundhofa@gmail.com>
2018-07-06 22:53:01 +0700

    tes

--- routes/controll_device_router.js
+++ routes/controll_device_router.js
@@ -9,8 +9,7 @@ var MotorSchema = require('../models/motor_model');
 router.get('/', checkAuth, function(req, res, next){
  async.series({
    data_relay: function(cb){
-    // MotorSchema.findById(req.userData.userId).exec(function(err, data){
-    MotorSchema.findById("5af166ddaf533a4b9c3fc0d6").exec(function(err, data){
+    MotorSchema.findById(req.userData.userId).exec(function(err, data){
      if(err) return console.log(err);
      cb(err, data.relay);
     });
