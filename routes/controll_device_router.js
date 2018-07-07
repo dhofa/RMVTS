95d2788eae842bd63853e0b7394388c8e7735bdf
dhofa <ahmadmundhofa@gmail.com>
2018-07-07 14:25:21 +0700

    test layout control device

--- routes/controll_device_router.js
+++ routes/controll_device_router.js
@@ -8,16 +8,17 @@ var MotorSchema = require('../models/motor_model');
 /* GET home page. */
 router.get('/', checkAuth, function(req, res, next){
  async.series({
-   data_relay: function(cb){
+   all_data: function(cb){
     MotorSchema.findById(req.userData.userId).exec(function(err, data){
      if(err) return console.log(err);
-     cb(err, data.relay);
+     cb(err, data);
     });
    }
   }, function(err, data){
    console.log(data.data_relay);
    res.render('controll_device_view',{
-    data_relay: data.data_relay
+    data_relay: data.all_data.relay,
+    last_driver_photos : data.all_data.vehicle_data.last_image_capture.last_driver_photos
    });
   });
 });
